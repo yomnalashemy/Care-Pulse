c18eea18496f5a2f213fc327d2bd2a92d95df03c
Spike Ice <spikeyice.1@gmail.com>
2025-07-02 21:32:27 +0300

    YASTA NAW NAW 3LA EL KALAM

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,9 +1,7 @@
 import * as sdk from "node-appwrite";
-import { ID } from "node-appwrite";
-import { revalidatePath } from "next/cache";
 
 export const {
-  NEXT_PUBLIC_APPWRITE_ENDPOINT: ENDPOINT,
+  NEXT_PUBLIC_ENDPOINT: ENDPOINT,
   PROJECT_ID,
   API_KEY,
   DATABASE_ID,
@@ -21,24 +19,3 @@ export const databases = new sdk.Databases(client);
 export const users = new sdk.Users(client);
 export const messaging = new sdk.Messaging(client);
 export const storage = new sdk.Storage(client);
-
-export const createAppointment = async (
-  appointment: CreateAppointmentParams
-) => {
-  try {
-    const generatedId = ID.unique();
-    console.log("Generated appointment ID:", generatedId);
-
-    const newAppointment = await databases.createDocument(
-      DATABASE_ID!,
-      APPOINTMENT_COLLECTION_ID!,
-      generatedId,
-      appointment
-    );
-
-    revalidatePath("/admin");
-    return newAppointment;
-  } catch (error) {
-    console.error("An error occurred while creating a new appointment:", error);
-  }
-};
